3b1100f0097ff9adc8cacf2e3d18d11f6a417303
gabrielleeyj <gabrielleeyj@outlook.com>
2021-11-29 10:42:06 +0800

    add Orderbook struct and function to print results

--- data/data.go
+++ data/data.go
@@ -1,26 +1,74 @@
 package data
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
 // based on the example input so the data.
 // The presumption is that the example inputs are unformatted strings.
 // I’ll parse the entire string and format it in memory to get the order format out first.
 
-orders := "A,100000,S,1,1075 A,100001,B,9,1000 A,100002,B,30,975 A,100003,S,10,1050 A,100004,B,10,950 A,100005,S,2,1025 A,100006,B,1,1000 X,100004,B,10,950 A,100007,S,5,1025 A,100008,B,3,1050 X,100008,B,3,1050 X,100005,S,2,1025"
-
-type Orders struct {
-	action  string
-	orderId int
-	side int
-	price int
+type Order struct {
+	Id       int64
+	Side     string
+	Quantity int64
+	Price    float64
 }
 
-func formatData(data) []string{
-	// split the string by space
-	order := strings.Split(data, " ")
-	return order
+type Orderbook struct {
+	BidPricePoints []float64
+	AskPricePoints []float64
+	Bids           map[float64][]Order
+	Asks           map[float64][]Order
 }
 
+func (ob *Orderbook) PrettyPrint() {
+
+	fmt.Println("===============================")
+
+	fmt.Println("ASKS")
+	//var a []float64
+	// (a, ob.askPricePoints))
+	for _, price := range reverse(copySlice(ob.AskPricePoints)) {
+		orders := ob.Asks[price]
+		if len(orders) == 0 {
+			continue
+		}
+		fmt.Printf("%f: ", price)
+		for _, order := range orders {
+			fmt.Printf("%d ", order.Quantity)
+			//fmt.Printf("%d-%d ", order.id, order.quantity)
+		}
+		fmt.Println()
+	}
+
+	fmt.Println("-------------------------------")
+
+	for _, price := range ob.BidPricePoints {
+		orders := ob.Bids[price]
+		if len(orders) == 0 {
+			continue
+		}
+		fmt.Printf("%f: ", price)
+		for _, order := range orders {
+			fmt.Printf("%d ", order.Quantity)
+			//fmt.Printf("%d-%d ", order.id, order.quantity)
+		}
+		fmt.Println()
+	}
+	fmt.Println("BIDS")
+
+	fmt.Println("===============================")
+
+}
+
+func reverse(s []float64) []float64 {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+	return s
+}
+
+func copySlice(s []float64) []float64 {
+	c := make([]float64, len(s))
+	copy(c, s)
+	return c
+}
