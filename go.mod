2b341585e2da1d2e5cf0fc436f32a7fc2a95100d
gabrielleeyj <gabrielleeyj@outlook.com>
2021-11-29 10:38:08 +0800

    add packages and dataset to run program

---
+++ go.mod
@@ -0,0 +1,3 @@
+module order-book-demo
+
+go 1.17
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
-package order_book_demo
+package main
+
+import (
+	"fmt"
+	"order-book-demo/data"
+)
+
+var book data.Orderbook
 
 func main() {
+	fmt.Println("Starting application...")
+	// init book to memory
+	book = data.Orderbook{}
+	book.Bids = map[float64][]data.Order{}
+	book.Asks = map[float64][]data.Order{}
 
+	// run the dataset of strings created to book and prints to console
+	readMarketData()
+	fmt.Println("Ending application...")
 }
